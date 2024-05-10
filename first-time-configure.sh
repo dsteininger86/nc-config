56121c6845c74af4400a50914a6c16e37fcaaaa4
Thomas Lehmann <t.lehmann@strato-rz.de>
2024-05-10 15:58:23 +0200

    first-time-configure: split into functions

--- first-time-configure.sh
+++ first-time-configure.sh
@@ -12,7 +12,7 @@ fail() {
 	exit 1
 }
 
-main() {
+checks() {
 	if ! which php 2>&1 >/dev/null; then
 		fail "Error: php is required"
 	fi
@@ -20,23 +20,34 @@ main() {
 	if ! which jq 2>&1 >/dev/null; then
 		fail "Error: jq is required"
 	fi
+}
 
+config_server() {
 	echo "Configure NextCloud basics"
-
-	if [ $(ooc status --output json | jq '.installed') != "true" ]; then
-		echo "NextCloud is not installed, abort"
-		exit 1
-	fi
 
 	ooc config:system:set lookup_server --value=""
 	ooc user:setting admin settings email admin@example.net
+}
 
+config_ui() {
 	echo "Configure theming"
 
 	ooc theming:config name "EasyStorage"
 	ooc theming:config color "#003D8F"
 	ooc theming:config disable-user-theming yes
 	ooc config:app:set theming backgroundMime --value backgroundColor
+}
+
+main() {
+	checks
+
+	if [ $(ooc status --output json | jq '.installed') != "true" ]; then
+		echo "NextCloud is not installed, abort"
+		exit 1
+	fi
+
+	config_server
+	config_ui
 }
 
 main ${@}
