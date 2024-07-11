72072e26a2bdc121215ffb1d40fd92b79c7d461e
Thomas Lehmann <t.lehmann@strato.de>
2024-07-11 15:01:55 +0200

    Add config partials for redirect, app paths, reduced langs
    * settings -> simplesettings redirects
    * app paths
    * reduced set of languages
    
    To be copied during image build.
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

---
+++ configs/app-paths.config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = [
+  'apps_paths' => [
+    [
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => true,
+    ],
+    [
+      'path' => '/var/www/html/apps-custom',
+      'url' => '/apps-custom',
+      'writable' => true,
+    ],
+    [
+      'path' => '/var/www/html/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ],
+  ],
+];
---
+++ configs/reduce-langs.config.php
@@ -0,0 +1,11 @@
+<?php
+$CONFIG = [
+  'reduce_to_languages' => [
+    'en',
+    'de',
+    'es',
+    'fr',
+    'it',
+    'nl',
+  ],
+];
