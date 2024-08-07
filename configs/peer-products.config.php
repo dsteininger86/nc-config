c42a652d2b629095ea0ef3637e12ab41682b9173
Franziska Bath <franziska.bath@strato.de>
2024-08-07 11:07:58 +0200

    feat(config): add help link to config
    Signed-off-by: Franziska Bath <franziska.bath@strato.de>

--- configs/peer-products.config.php
+++ configs/peer-products.config.php
@@ -2,5 +2,6 @@
 $CONFIG = [
   'ionos_peer_products' => [
 	'ionos_webmail_target_link' => 'https://email.ionos.fr/',
-  ]
+  ],
+  'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0027',
 ];
