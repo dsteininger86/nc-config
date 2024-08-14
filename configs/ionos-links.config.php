fb8df42be652a4029c4184b72ec1dbe8358ea08c
Kai Henseler <kai.henseler@strato.de>
2024-08-14 15:58:42 +0200

    feat: add custom client config parameters
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

---
+++ configs/ionos-links.config.php
@@ -0,0 +1,12 @@
+<?php
+$CONFIG = [
+  'ionos_peer_products' => [
+	'ionos_webmail_target_link' => 'https://email.ionos.fr/',
+  ],
+  'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0027',
+  'ionos_customclient_windows' => 'https://wl.hidrive.com/easy/0003',
+  'ionos_customclient_macos' => 'https://wl.hidrive.com/easy/0003',
+  'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0022',
+  'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/0021',
+  'ionos_customclient_ios_appid' => '1439073561',
+];
--- configs/peer-products.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$CONFIG = [
-  'ionos_peer_products' => [
-	'ionos_webmail_target_link' => 'https://email.ionos.fr/',
-  ],
-  'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0027',
-];
