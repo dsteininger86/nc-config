a9a47eeb9a7b354383f288c8c95cd049d02181b2
Thomas Lehmann <t.lehmann@strato.de>
2024-10-24 13:31:24 +0200

    feat(config): disable federated sharing
    With this config enabled users could enter user@host ID of external
    Nextcloud instances. This should be disallowed for the MVP to avoid
    unforseen consequences and to keep it simple.
    
    The config "outgoing_server2server_share_enabled" was found by reverse
    engineering the user lookup in the share UI.
    
    The other configs were found by systematic search in the app source:
    
       grep -RE 'getApp.*files_sharing' apps/federatedfilesharing/
    
    Signed-off-by: Thomas Lehmann <t.lehmann@strato.de>

--- configure.sh
+++ configure.sh
@@ -42,8 +42,20 @@ config_ui() {
 }
 
 config_apps() {
-	echo "Configure apps"
+	echo "Configure apps ..."
+
+	echo "Configure viewer app"
 	ooc config:app:set --value yes --type string viewer always_show_viewer
+
+	echo "Disable federated sharing"
+	# To disable entering the user@host ID of an external Nextcloud instance
+	# in the (uncustomized) search input field of the share panel
+	ooc config:app:set --value no files_sharing outgoing_server2server_share_enabled
+	ooc config:app:set --value no files_sharing incoming_server2server_share_enabled
+	ooc config:app:set --value no files_sharing outgoing_server2server_group_share_enabled
+	ooc config:app:set --value no files_sharing incoming_server2server_group_share_enabled
+	ooc config:app:set --value no files_sharing lookupServerEnabled
+	ooc config:app:set --value no files_sharing lookupServerUploadEnabled
 }
 
 add_config_partials() {
