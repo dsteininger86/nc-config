a68e5b755ef7649c8c043eb01a456509a05ec22c
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-01-29 10:49:27 +0100

    feature(admin_audit): add admin_audit config values
    https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/logging_configuration.html#admin-audit-log-optional
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ configs/admin_audit.config.php
@@ -0,0 +1,10 @@
+<?php
+
+$CONFIG = [
+	'log.condition' => [
+		'apps' => ['admin_audit'],
+	],
+	"log_type_audit" => "errorlog",
+	"syslog_tag_audit" => "hidrivenext_admin_audit",
+	"logfile_audit" => "",
+];
