4ac235b0ac66e904d01e6f05a63fa647e5a4de40
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-02-04 16:48:40 +0100

    feat(allow-apps-useragents): allow user agents of desired apps in login-flow-v2
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ configs/ionos-apps-user-agents.config.php
@@ -0,0 +1,8 @@
+<?php
+$CONFIG = [
+	'core.login_flow_v2.allowed_user_agents' => [
+		// Use regex to match the user agent string
+		// to allow the login flow for allowed apps
+		'/Hidrive Next/i',
+	],
+];
