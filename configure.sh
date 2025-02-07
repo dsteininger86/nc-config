815d9caebe62b1737ed48a4ce6e076e4906d8ef0
Franziska Bath <franziska.bath@strato.de>
2025-02-07 16:38:02 +0100

    feat(config): update favicon
    Signed-off-by: Franziska Bath <franziska.bath@strato.de>

--- configure.sh
+++ configure.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 BDIR="$( dirname "${0}" )"
-ABSOLUTE_BDIR="$(cd $(dirname $BDIR); pwd)/$(basename $BDIR)"
+NEXTCLOUD_DIR="${BDIR}/.."
+FAVICON_DIR=$(cd "${NEXTCLOUD_DIR}/apps-custom/nc_theming/img" && pwd)
 ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
 ADMIN_EMAIL=${ADMIN_EMAIL:-admin@example.net}
 
@@ -36,7 +37,7 @@ config_ui() {
 	ooc theming:config name "HiDrive Next"
 	ooc theming:config primary_color "#003D8F"
 	ooc theming:config disable-user-theming yes
-	ooc theming:config favicon $ABSOLUTE_BDIR/favicon.ico
+	ooc theming:config favicon "${FAVICON_DIR}/favicon.ico"
 	ooc config:app:set theming backgroundMime --value backgroundColor
 	ooc config:system:set defaultapp --value files
 }
